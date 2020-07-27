de749a525755383cea8dd5a5ed789b55b34d7a74
Fanny Sabin <fannysabin.fs@gmail.com>
2020-07-27 17:08:19 +0200

    adding map

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-const Home = (props) => {
-  // Implement react map box here.
-  return (
-    <div>
-      <h1>MAPBOX MAP HERE</h1>
-      <p>On home /</p>
-    </div>
-  );
-};
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
-export default Home;
+export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 2.351027,
+      lat: 48.855,
+      zoom: 11.7
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{position: "absolute", top: "0", right: "0", left: "0",
+bottom: "0", zIndex:"-20"}} ref={el => this.mapContainer = el} />
+        <h1>MAPBOX MAP HERE</h1>
+        <p>On home /</p>
+      </div>
+    )
+  }
+}
+
+export default Home
