63e8ec8c5e5668b3996a737637e563ad4b8bffc8
cchakibb <chakibbachir88@gmail.com>
2020-07-28 09:27:36 +0200

    modify Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import mapboxgl from 'mapbox-gl';
+import React, { Component } from "react";
+import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
@@ -7,31 +7,30 @@ export class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lng: 2.351027, 
+      lng: 2.351027,
       lat: 48.855,
-      zoom: 11.7
+      zoom: 11,
     };
   }
 
   componentDidMount() {
     const map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: "mapbox://styles/mapbox/streets-v11",
       center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
+      zoom: this.state.zoom,
     });
   }
 
   render() {
     return (
       <div>
-        <div style={{position: "absolute", top: "170px", right: "0", left: "0",
-bottom: "0", zIndex:"0"}} ref={el => this.mapContainer = el} />
+        <div style={{ position: "absolute", top: "170px", right: "0", left: "0", bottom: "0", zIndex: "0" }} ref={(el) => (this.mapContainer = el)} />
         <h1>MAPBOX MAP HERE</h1>
         <p>On home /</p>
       </div>
-    )
+    );
   }
 }
 
-export default Home
+export default Home;
